dd00de1c479db0ea710f8c16fe95f83718d15f75
hashiguchp <hashiguchip@gmail.com>
2019-11-04 20:55:47 +0900

    feat: Fist commit

---
+++ manifest.js
@@ -0,0 +1,38 @@
+var aaa = {
+    manifest_version: 2,
+    version: '0.0.1',
+    name: 'hashiguchip',
+    theme: {
+        images: {
+            // main
+            theme_frame: './images/black.png',
+            theme_frame_overlay: './images/black.jpg',
+            theme_frame_incognito: './images/main.png',
+            theme_frame_incognito_overlay: './images/main.png',
+            theme_toolbar: './images/main.png',
+            theme_ntp_background: './images/background.png',
+            theme_ntp_attribution: './images/main.png',
+        },
+        colors: {
+            frame: [71, 105, 91],
+            toolbar: [207, 221, 192],
+            ntp_text: [20, 40, 0],
+            ntp_link: [36, 70, 0],
+            ntp_section: [207, 221, 192],
+            button_background: [255, 255, 255],
+        },
+        tints: {
+            frame: [-1.0, -1.0, -1.0],
+            frame_inactive: [-1.0, -1.0, -1.0],
+            frame_incognito: [-1.0, -1.0, -1.0],
+            frame_incognito_inactive: [-1.0, -1.0, -1.0],
+            background_tab: [-1.0, -1.0, -1.0],
+            buttons: [0.33, 0.5, 0.47],
+        },
+        properties: {
+            ntp_background_alignment: 'center',
+            ntp_background_repeat: 'no-repeat',
+            ntp_logo_alternate: 1,
+        },
+    },
+};
